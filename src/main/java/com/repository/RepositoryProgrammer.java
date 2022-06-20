90bcb015fafd3723f4daec1884d9defecfdb6931
Vasilenko Ilya <vasilenko.lesnik2017@yandex.ru>
2022-06-21 00:28:34 +0900

    pet-clinick-project

--- src/main/java/com/repository/RepositoryProgrammer.java
+++
@@ -1,7 +0,0 @@
-package com.repository;
-
-import com.domain.Programmer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RepositoryProgrammer extends CrudRepository<Programmer, Integer> {
-}
