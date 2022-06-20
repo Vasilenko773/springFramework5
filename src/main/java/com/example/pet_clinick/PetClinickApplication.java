ff5ad6dd0792f6b2957a414cf4ba2b4465436e16
Vasilenko Ilya <vasilenko.lesnik2017@yandex.ru>
2022-06-21 00:19:22 +0900

    pet-clinick-project

---
+++ src/main/java/com/example/pet_clinick/PetClinickApplication.java
@@ -0,0 +1,13 @@
+package com.example.pet_clinick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinickApplication.class, args);
+    }
+
+}
