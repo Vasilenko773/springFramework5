825836aacb422cfab7f99f32034965703de3aae0
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-07-09 12:51:02 +0900

    presentation layer vets

---
+++ project-web/src/main/java/ru/controllers/VetController.java
@@ -0,0 +1,13 @@
+package ru.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping("/vets")
+    public String index() {
+        return "vets/index";
+    }
+}
