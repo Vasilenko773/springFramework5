a0c817fa0cf61cab8d090d4d2f547c9c26bc1852
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-07-18 17:56:35 +0900

    spring boot configuration fakeDataSource bean

--- project-data/src/main/java/data/config/ConfigInDataBase.java
+++ project-data/src/main/java/data/config/ConfigInDataBase.java
@@ -1,13 +1,12 @@
 package data.config;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+@PropertySource("classpath:application.properties")
 public class ConfigInDataBase {
 
     @Bean
