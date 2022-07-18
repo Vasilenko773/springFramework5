0959c91127d338bea1270081cdaee5f0cb5a949c
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-07-18 13:39:23 +0900

    list vet thymeleaf

--- project-web/src/main/java/web/controllers/VetController.java
+++ project-web/src/main/java/web/controllers/VetController.java
@@ -1,13 +1,22 @@
 package web.controllers;
 
+import data.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping("/vets")
-    public String index() {
-        return "vets/index";
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping("/vet")
+    public String index(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+        return "vet/index";
     }
 }
