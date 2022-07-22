6e78d13a8897d3ecc16cbc480a9277719d50fc05
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-07-23 00:31:13 +0900

    owner page update title style

--- project-web/src/main/java/web/General.java
+++ project-web/src/main/java/web/General.java
@@ -1,24 +1,13 @@
 package web;
 
-import data.config.FakeDataSource;
-import data.config.ClassConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication(scanBasePackages = {"data", "web"})
 public class General {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(General.class, args);
-
-        System.out.println("FakeDataSource.class");
-        FakeDataSource fakeDataSource = context.getBean(FakeDataSource.class);
-        System.out.println(fakeDataSource.getPassword());
-
-        System.out.println("ClassConfiguration.class");
-        ClassConfiguration classConfiguration = context.getBean(ClassConfiguration.class);
-        System.out.println(classConfiguration.getUrl());
+      SpringApplication.run(General.class, args);
     }
 }
